a1abd432745d08a95a40e1cf33b200ceb8672c55
platy <mike@relayr.io>
2015-01-26 10:22:32 +0100

    Utility for creating timeout futures

---
+++ src/test/scala/io/relayr/amqp/concurrent/FutureTimeoutSpec.scala
@@ -0,0 +1,23 @@
+package io.relayr.amqp.concurrent
+
+import java.util.concurrent.TimeoutException
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class FutureTimeoutSpec extends FlatSpec with Matchers {
+
+  implicit val executor = new ScheduledExecutor(1)
+
+  "FutureTimeout" should "allow a quick enough operation to complete" in {
+    val future = FutureTimeout.timeoutAfter(20 millis)(executor.delayExecution("short")(10 milliseconds))
+    Await.result(future, 100 millis) should be ("short")
+  }
+
+  it should "timeout longer operations" in {
+    val future = FutureTimeout.timeoutAfter(10 millis)(executor.delayExecution("long")(50 milliseconds))
+    intercept[TimeoutException](Await.result(future, 100 millis))
+  }
+}
---
+++ src/test/scala/io/relayr/amqp/concurrent/ScheduledExecutorSpec.scala
@@ -0,0 +1,44 @@
+package io.relayr.amqp.concurrent
+
+import java.util.concurrent.TimeoutException
+
+import org.scalatest.{ Matchers, FlatSpec }
+
+import scala.concurrent.{ Future, Await }
+import scala.concurrent.duration._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ScheduledExecutorSpec extends FlatSpec with Matchers {
+
+  val executor = new ScheduledExecutor(1)
+
+  "Scheduled operations" should "not complete too early" in {
+    intercept[TimeoutException](Await.ready(executor.delayExecution(())(50 milliseconds), 10 milliseconds))
+  }
+
+  it should "not take too long" in {
+    val ready: Future[Unit] = Await.ready(executor.delayExecution(())(10 milliseconds), 50 milliseconds)
+    ready.isCompleted should be (true)
+  }
+
+  it should "complete in order" in {
+    val future = Future.firstCompletedOf(Seq[Future[String]](
+      executor.delayExecution("short")(10 milliseconds),
+      executor.delayExecution("long")(50 milliseconds)))
+    Await.result(future, 100 millis) should be ("short")
+  }
+
+  it should "complete out of order" in {
+    val future = Future.firstCompletedOf(Seq[Future[String]](
+      executor.delayExecution("long")(50 milliseconds),
+      executor.delayExecution("short")(10 milliseconds)))
+    Await.result(future, 100 millis) should be ("short")
+  }
+
+  it should "not complete after cancellation" in {
+    val execution = executor.delayExecution(())(10 milliseconds)
+    execution.cancel(false)
+    intercept[TimeoutException](Await.ready(execution, 50 milliseconds))
+  }
+}
