6f40e77ee6f9d5de15a981bd0c34afaf7042c9a3
platy <mike@relayr.io>
2015-01-28 14:40:45 +0100

    Use reconnection built into the java client

--- src/main/scala/io/relayr/amqp/connection/ConnectionHolderFactory.scala
+++ src/main/scala/io/relayr/amqp/connection/ConnectionHolderFactory.scala
@@ -1,7 +1,7 @@
 package io.relayr.amqp.connection
 
 import com.rabbitmq.client.ConnectionFactory
-import io.relayr.amqp.ReconnectionStrategy.JavaClientFixedReconnectDelay
+import io.relayr.amqp.ReconnectionStrategy.{ NoReconnect, JavaClientFixedReconnectDelay }
 import io.relayr.amqp.{ ConnectionHolder, EventHooks, ReconnectionStrategy }
 
 import scala.concurrent.ExecutionContext
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext
 private[amqp] class ConnectionHolderFactory(connectionFactory: ConnectionFactory, reconnectionStrategy: ReconnectionStrategy, eventHooks: EventHooks, executionContext: ExecutionContext) {
   def newConnectionHolder(): ConnectionHolder = {
     reconnectionStrategy match {
-      case noReconnect ⇒ ()
+      case NoReconnect ⇒ ()
       case JavaClientFixedReconnectDelay(networkRecoveryInterval) ⇒
         connectionFactory.setAutomaticRecoveryEnabled(true)
         connectionFactory.setNetworkRecoveryInterval(networkRecoveryInterval.toMillis)
